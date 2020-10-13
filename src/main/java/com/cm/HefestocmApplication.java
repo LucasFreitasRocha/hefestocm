752ac571564b0fb78bcda8a950b752a14477e067
Lucas Rocha <rochadefreitaslucas@gmail.com>
2020-10-13 20:53:59 -0300

    initial config

---
+++ src/main/java/com/cm/HefestocmApplication.java
@@ -0,0 +1,13 @@
+package com.cm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HefestocmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HefestocmApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cm/domain/Pedido.java
@@ -0,0 +1,5 @@
+package com.cm.domain;
+
+public class Pedido {
+
+}
