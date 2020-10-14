fc49b1e9dfa353d08c8f101c75560161df964b61
Lucas Rocha <rochadefreitaslucas@gmail.com>
2020-10-13 23:05:15 -0300

    criando o resto da estrutura

--- src/main/java/com/cm/domain/Pedido.java
+++ src/main/java/com/cm/domain/Pedido.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import com.cm.domain.enums.StatusPedido;
 
+@Entity
 public class Pedido implements Serializable {
 
 	private static final long serialVersionUID = 1L;
